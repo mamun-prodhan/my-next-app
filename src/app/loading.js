47df6cdeaf38fbfef5542ec7e3b8cab227ef72a1
mamun-prodhan <mamunpr9@gmail.com>
2023-12-07 17:35:09 +0600

    error notfound loading page implemented

---
+++ src/app/loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-center text-red-600 my-20">
+        Loading.......
+      </h2>
+    </div>
+  );
+};
+
+export default LoadingPage;
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h2>ooops....404!!! page not found</h2>
+    </div>
+  );
+};
+
+export default NotFoundPage;
